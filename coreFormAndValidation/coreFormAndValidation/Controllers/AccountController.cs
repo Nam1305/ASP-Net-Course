9282115f46327e982f3f9020f073e87efaa59a21
Nam1305 <Nbnam113@gmail.com>
2025-05-20 20:58:09 +0700

    1

---
+++ coreFormAndValidation/coreFormAndValidation/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using coreFormAndValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreFormAndValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult WeeklyTypedLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginPost(string username, string password)
+        {
+            ViewBag.Username = username;    
+            ViewBag.Password = password;
+            return View();
+        }
+
+        public IActionResult StronglyTypedLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginSuccess(LoginViewModel login)
+        {
+            ViewBag.Username = login.Username;
+            ViewBag.Password = login.Password;
+            return View();
+        }
+
+        public IActionResult UserDetail()
+        {
+            var user = new LoginViewModel()
+            {
+                Username = "Pham Hoang Nam",
+                Password = "12345"
+            };
+            return View(user);
+        }
+
+        public IActionResult UserList()
+        {
+            var users = new List<LoginViewModel>()
+            {
+                new LoginViewModel(){Username = "Pham Hoang 1",Password = "12345"},
+                new LoginViewModel(){Username = "Pham Hoang 2",Password = "12345"},
+                new LoginViewModel(){Username = "Pham Hoang 3",Password = "12345"},
+                new LoginViewModel(){Username = "Pham Hoang 4",Password = "12345"},
+
+            };
+            return View(users);
+        }
+    }
+}
---
+++ coreFormAndValidation/coreFormAndValidation/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace coreFormAndValidation.Models
+{
+    public class LoginViewModel
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
